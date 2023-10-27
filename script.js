f2dde357c99b84acda9a68c42a660daeaaa421b3
Lusa-mata <124500463+Lusa-mata@users.noreply.github.com>
2023-10-27 14:21:36 +0200

    removed back to top button

--- script.js
+++ script.js
@@ -15,19 +15,3 @@ if (window.innerWidth > 768) {
 });
 
 
-
-
-window.onscroll = function() { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("back-to-top-button").style.display = "block";
-    } else {
-        document.getElementById("back-to-top-button").style.display = "none";
-    }
-}
-
-document.getElementById("back-to-top-button").addEventListener("click", function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});

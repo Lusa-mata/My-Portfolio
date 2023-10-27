e670676be288c55736c0330004a165f0c122d560
Lusa-mata <124500463+Lusa-mata@users.noreply.github.com>
2023-10-27 14:07:41 +0200

    back-to-top

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ if (window.innerWidth > 768) {
     menu.classList.remove("active");
     }
 });
+
+
+
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top-button").style.display = "block";
+    } else {
+        document.getElementById("back-to-top-button").style.display = "none";
+    }
+}
+
+document.getElementById("back-to-top-button").addEventListener("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});

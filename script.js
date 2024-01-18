00c3b93389d568acae78c459f98ba029d0bb1327
Lusa-mata <124500463+Lusa-mata@users.noreply.github.com>
2024-01-18 14:57:39 +0200

    updated title

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.addEventListener("resize", () => {
 
 
 // Typing and erasing animation
-const words = ["Hi there, I am Lusaduma Mata ", "A software developer intern "];
+const words = ["Hi there, I am Lusaduma Mata ", "A Junior Software Developer ", "Front-end Web Developer"];
 let index = 0;
 let isTyping = true;
 let currentWord = "";
